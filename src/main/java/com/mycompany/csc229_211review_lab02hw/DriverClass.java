3b8cf5669951a1fdcc1fb6caff5cda95cce1ca25
CPSage1 <christianpaucarcane@gmail.com>
2024-02-10 23:43:01 -0500

    Added my comments

--- src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
@@ -1,28 +1,37 @@
 package com.mycompany.csc229_211review_lab02hw;
-
+import java.util.Scanner;
 /**
  *
  * @author MoaathAlrajab
  */
 public class DriverClass {
-	
+
 	public static void main(String[] args) {
 		// ToDo 5: Fix the error
-		
-		// ToDo 6: Fix the constructor of Student class
-		
-		// ToDo 7: Add a toString method for Student class
-		
-		Student std1= new Student("James", 20);
-		
-		// ToDo 8: Set the gpa of the student using the scanner and user
-		// 			input and then print the output.
-		
-		System.out.println(std1);
-		
-		// ToDo 9: add comments and explain your code
-		
-		// ToDo 10: submit using a pull request.
-	}
+		// Fixed the Student class with proper arguments
 
+		// ToDo 6: Fix the constructor of Student class
+		//Check out the Student Class
+		Student std1 = new Student("Bob Parr", (short) 20);
+
+		// ToDo 8: Set the GPA of the student using the scanner and user
+		// input and then print the output.
+		Scanner scanner = new Scanner(System.in);
+
+		// This section tells the user to input the student's GPA
+
+		System.out.print("Enter student's GPA: ");
+		// sets the GPA for the student object, and then prints it out
+		double gpa = scanner.nextDouble();
+		// the students information(name, age, and GPA).
+		std1.setGpa(gpa);
+
+		// ToDo 9: Add comments and explain your code
+
+
+
+		System.out.println(std1);
+
+		// ToDo 10: Submit using a pull request.
+	}
 }
--- src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
@@ -4,13 +4,49 @@ package com.mycompany.csc229_211review_lab02hw;
  *
  * @author MoaathAlrajab
  */
-public class Student {
-	// ToDo 1: Make this class a child of Person
-	
-	// ToDo 2: Fix the resulting errors
-	
-	// ToDo 3: Add a field for GPA and create setter and getter
-	
-	// ToDo 4: Add comments to your code
+// ToDo 1: Make this class a child of Person
+public class Student extends Person {
 
-}
+    // Field for GPA
+    // ToDo 3: Add a field for GPA and create setter and getter
+    private double gpa;
+
+    // ToDo 2: Fix the resulting errors
+
+    // The Constructor
+    // ToDo 6: Fix the constructor of Student class
+    public Student(String name, short age) {
+        super(name, age);
+    }
+
+    // Implementing the getAddress from the Person class
+    @Override
+    public String getAddress() {
+        return "";
+    }
+
+    // Implementing the setAddress from the Person class
+    @Override
+    public void setAddress(String address) {
+    }
+    // ToDo 3: Add a field for GPA and create setter and getter
+    // Getter for GPA
+    public double getGpa() {
+        return gpa;
+    }
+
+    // Setter for GPA
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+    // ToDo 7: Add a toString method for Student class
+    // ToString method to show the students information
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + getName() + '\'' +
+                ", age=" + getAge() +
+                ", GPA=" + gpa +
+                '}';
+    }
+}// ToDo 4: Add comments to your code
